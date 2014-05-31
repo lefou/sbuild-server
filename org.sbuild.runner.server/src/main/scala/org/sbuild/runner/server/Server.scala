bbe8191952c5630dc0336177c24754d253358595
Tobias Roeser <le.petit.fou@web.de>
2014-05-31 11:56:41 +0200

    Added Spray app stub.

--- org.sbuild.runner.server/src/main/scala/org/sbuild/runner/server/Server.scala
+++ org.sbuild.runner.server/src/main/scala/org/sbuild/runner/server/Server.scala
@@ -1,5 +1,20 @@
 package org.sbuild.runner.server
 
-class Server() {
+import akka.actor.ActorSystem
+import spray.routing.SimpleRoutingApp
 
+
+object Server extends App with SimpleRoutingApp {
+
+  implicit val actorSystem = ActorSystem()
+
+  startServer(interface = "localhost", port = 8080) {
+    post {
+      path("run") {
+        complete {
+          "Nothing done"
+        }
+      }
+    }
+  }
 }
