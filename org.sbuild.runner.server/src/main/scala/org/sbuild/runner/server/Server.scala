fe862d536142953a7adc017715f3f61b2e3bd7e0
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2014-05-31 14:01:02 +0200

    some chunked response test

--- org.sbuild.runner.server/src/main/scala/org/sbuild/runner/server/Server.scala
+++ org.sbuild.runner.server/src/main/scala/org/sbuild/runner/server/Server.scala
@@ -1,38 +1,72 @@
 package org.sbuild.runner.server
 
-import akka.actor.ActorSystem
-import spray.routing.SimpleRoutingApp
-import spray.httpx.unmarshalling.Unmarshaller
-import spray.http.HttpEntity
+import akka.actor._
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+import scala.concurrent.Await
+import akka.io.IO
+import spray.can.Http
+import scala.util.{Failure, Success}
+import spray.http._
+import HttpMethods._
+import spray.can.Http.{Connected, Register}
+import spray.http.HttpRequest
+import spray.http.HttpResponse
+import scala.util.Success
+import spray.can.Http.Register
+import scala.util.Failure
+import org.sbuild.runner.server.HttpConfig
 
+case object GetHttpConfig
+case class HttpConfig(host: String, port: Int)
 
-object Server extends App with SimpleRoutingApp {
+object Server extends App {
+  implicit val system = ActorSystem()
 
-    implicit val actorSystem = ActorSystem()
+  // the handler actor replies to incoming HttpRequests
+  val handler = system.actorOf(Props[Foo], "coffee-service")
 
-    private val serverPort = 8080
+  import system.dispatcher
+  implicit val timeout = Timeout(5.seconds)
 
-    startServer(interface = "0.0.0.0", port = serverPort) {
-        post {
-            path("echo") {
-                entity(as[String]) {
-                    parameters =>
-                        println(parameters.split("&").toList)
-                        complete {
-                            s"http://localhost:$serverPort/echo/1234"
-                        }
-
-                }
-
-            }
-        } ~
-            get {
-                path("echo" / Rest) {
-                    id =>
-                        complete {
-                            s"log for run of $id"
-                        }
-                }
-            }
-    }
+  (handler ? GetHttpConfig).onComplete {
+    case Success(HttpConfig(host, port)) =>
+      IO(Http) ! Http.Bind(handler, host, port, 1000)
+    case Success(s) =>
+      throw new IllegalStateException(s"Unexpected result: $s")
+    case Failure(t) =>
+      t.printStackTrace()
+  }
 }
+
+//object Server extends App with SimpleRoutingApp {
+//
+//  implicit val actorSystem = ActorSystem()
+//
+//  private val serverPort = 8080
+//
+//  startServer(interface = "0.0.0.0", port = serverPort) {
+//    post {
+//      path("echo") {
+//        entity(as[String]) {
+//          parameters =>
+//            println(parameters.split("&").toList)
+//            complete {
+//              s"http://localhost:$serverPort/echo/1234"
+//            }
+//
+//        }
+//
+//      }
+//    } ~
+//      get {
+//        path("echo" / Rest) {
+//          id =>
+//            complete {
+//              s"log for run of $id"
+//            }
+//        }
+//      }
+//  }
+//}
